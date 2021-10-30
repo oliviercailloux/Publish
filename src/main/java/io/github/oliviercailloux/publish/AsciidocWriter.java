4176b2714134991f5a586f7abc776dd08ac67194
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-10-30 18:23:51 +0200

    Test main in AsciidocWriter just to check JRuby

--- src/main/java/io/github/oliviercailloux/publish/AsciidocWriter.java
+++ src/main/java/io/github/oliviercailloux/publish/AsciidocWriter.java
@@ -4,19 +4,37 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.Streams;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.OptionsBuilder;
+import org.asciidoctor.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO should sanitize input.
  */
 public class AsciidocWriter {
+  @SuppressWarnings("unused")
+  private static final Logger LOGGER = LoggerFactory.getLogger(AsciidocWriter.class);
+
+  public static void main(String[] args) {
+    final AsciidocWriter writer = new AsciidocWriter();
+    final String content = "a *starred* on\ntwo *`lines`*!";
+    writer.verbatim(content);
+    final String written = writer.getContent();
+
+    LOGGER.info("Creating.");
+    try (Asciidoctor adocConverter = Asciidoctor.Factory.create()) {
+      LOGGER.info("Created.");
+      final String adocXml =
+          adocConverter.convert(written, Options.builder().backend("docbook").build());
+      LOGGER.debug("Xml: {}.", adocXml);
+    }
+  }
+
   public static String quote(String text) {
     if (text.isEmpty()) {
       return "";
