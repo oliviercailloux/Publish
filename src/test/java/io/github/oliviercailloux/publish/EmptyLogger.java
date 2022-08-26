769d760dde17b263d0aeae1e0e5b41491f9d0630
Olivier Cailloux <olivier.cailloux@gmail.com>
2022-08-26 21:14:42 +0200

    JRuby failing and succeeding

--- src/test/java/io/github/oliviercailloux/publish/EmptyLogger.java
+++ src/test/java/io/github/oliviercailloux/publish/EmptyLogger.java
@@ -1,81 +1,91 @@
 package io.github.oliviercailloux.publish;
 
+import java.io.PrintStream;
 import org.jruby.util.log.Logger;
 
 public class EmptyLogger implements Logger {
 
-  @Override
-  public String getName() {
-    return "name";
-  }
+	public EmptyLogger(@SuppressWarnings("unused") String loggerName) {
+	}
 
-  @Override
-  public void warn(String message, Object... args) {
+	public EmptyLogger(@SuppressWarnings("unused") Class<?> loggerClass) {
+	}
 
-  }
+	public EmptyLogger(@SuppressWarnings("unused") String loggerName, @SuppressWarnings("unused") PrintStream stream) {
+	}
 
-  @Override
-  public void warn(Throwable throwable) {
+	@Override
+	public String getName() {
+		return "name";
+	}
 
-  }
+	@Override
+	public void warn(String message, Object... args) {
 
-  @Override
-  public void warn(String message, Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void warn(Throwable throwable) {
 
-  @Override
-  public void error(String message, Object... args) {
+	}
 
-  }
+	@Override
+	public void warn(String message, Throwable throwable) {
 
-  @Override
-  public void error(Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void error(String message, Object... args) {
 
-  @Override
-  public void error(String message, Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void error(Throwable throwable) {
 
-  @Override
-  public void info(String message, Object... args) {
+	}
 
-  }
+	@Override
+	public void error(String message, Throwable throwable) {
 
-  @Override
-  public void info(Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void info(String message, Object... args) {
 
-  @Override
-  public void info(String message, Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void info(Throwable throwable) {
 
-  @Override
-  public void debug(String message, Object... args) {
+	}
 
-  }
+	@Override
+	public void info(String message, Throwable throwable) {
 
-  @Override
-  public void debug(Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void debug(String message, Object... args) {
 
-  @Override
-  public void debug(String message, Throwable throwable) {
+	}
 
-  }
+	@Override
+	public void debug(Throwable throwable) {
 
-  @Override
-  public boolean isDebugEnabled() {
-    return false;
-  }
+	}
 
-  @Override
-  public void setDebugEnable(boolean debug) {
+	@Override
+	public void debug(String message, Throwable throwable) {
 
-  }
+	}
+
+	@Override
+	public boolean isDebugEnabled() {
+		return false;
+	}
+
+	@Override
+	public void setDebugEnable(boolean debug) {
+
+	}
 }
--- src/test/java/io/github/oliviercailloux/publish/JRubyTests.java
+++ src/test/java/io/github/oliviercailloux/publish/JRubyTests.java
@@ -1,22 +1,34 @@
 package io.github.oliviercailloux.publish;
 
+import com.google.common.collect.ImmutableMap;
 import org.jruby.Ruby;
+import org.jruby.RubyInstanceConfig;
+import org.jruby.util.log.SLF4JLogger;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * See https://github.com/jruby/jruby/issues/6925
+ */
 public class JRubyTests {
-  @SuppressWarnings("unused")
-  private static final Logger LOGGER = LoggerFactory.getLogger(JRubyTests.class);
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(JRubyTests.class);
 
-  /**
-   * See https://github.com/jruby/jruby/issues/6925
-   */
-  @Test
-  void testJRuby() throws Exception {
-    LOGGER.info("Starting ruby.");
-    System.setProperty("jruby.logger.class", EmptyLogger.class.getCanonicalName());
-    Ruby.newInstance();
-    LOGGER.info("Started ruby.");
-  }
+	@Test
+	void testRedirectLoggingWorks() throws Exception {
+		LOGGER.info("Starting ruby.");
+		System.setProperty("jruby.logger.class", SLF4JLogger.class.getCanonicalName());
+		Ruby.newInstance();
+		LOGGER.info("Started ruby.");
+	}
+
+	@Test
+	void testRedirectLoggingFails() throws Exception {
+		LOGGER.info("Starting ruby.");
+		final RubyInstanceConfig config = new RubyInstanceConfig();
+		config.setEnvironment(ImmutableMap.of("jruby.logger.class", SLF4JLogger.class.getCanonicalName()));
+		Ruby.newInstance(config);
+		LOGGER.info("Started ruby.");
+	}
 }
