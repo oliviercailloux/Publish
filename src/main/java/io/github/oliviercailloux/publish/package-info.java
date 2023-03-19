19467dafea35e3b4f19bfa9fa28ca2afa3fdbf7f
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-03-19 18:28:59 +0100

    Pck info

---
+++ src/main/java/io/github/oliviercailloux/publish/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * Publish tools.
+ * <p>
+ * Hyphenation files are used with permission, see
+ * https://svn.lal.in2p3.fr/LCG/QWG/External/offo-hyphenation-fop-1.0/licenses.html,
+ * https://opensource.stackexchange.com/q/10426, and https://markmail.org/message/scd3ciflggk6e5rs.
+ * </p>
+ *
+ * <p>
+ * Every methods in this library (unless explicitly specified) are
+ * <a href="https://github.com/google/guava/wiki/UsingAndAvoidingNullExplained">null hostile</a>:
+ * they throw {@link java.lang.NullPointerException} if given some {@code null} argument, and never
+ * return {@code null} values.
+ * </p>
+ *
+ * <p>
+ * This library does not <a href=
+ * "https://objectcomputing.com/resources/publications/sett/january-2010-reducing-boilerplate-code-with-project-lombok#sneaky">sneaky-throw</a>.
+ * </p>
+ *
+ * <p>
+ * This library makes no guarantee about serializability.
+ * </p>
+ *
+ * <p>
+ * Please <a href="https://www.github.com/oliviercailloux/Publish/issues">open issues</a> if you
+ * find bugs or have feature requests. I will consider the bug reports seriously.
+ * </p>
+ */
+package io.github.oliviercailloux.publish;
