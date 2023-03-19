aa0aaa8d4f65111f0d1443716d875872e94a0fb5
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-03-19 01:12:48 +0100

    Doc defaults DB CC

--- src/main/java/io/github/oliviercailloux/publish/DocBookConformityChecker.java
+++ src/main/java/io/github/oliviercailloux/publish/DocBookConformityChecker.java
@@ -1,5 +1,6 @@
 package io.github.oliviercailloux.publish;
 
+import com.google.common.base.VerifyException;
 import com.thaiopensource.relaxng.jaxp.XMLSyntaxSchemaFactory;
 import io.github.oliviercailloux.jaris.xml.ConformityChecker;
 import io.github.oliviercailloux.jaris.xml.SchemaHelper;
@@ -14,15 +15,36 @@ public class DocBookConformityChecker {
   @SuppressWarnings("unused")
   private static final Logger LOGGER = LoggerFactory.getLogger(DocBookConformityChecker.class);
 
-  public static final SchemaFactory SCHEMA_FACTORY = new XMLSyntaxSchemaFactory();
   public static final StreamSource SCHEMA_SOURCE =
       new StreamSource(DocBookConformityChecker.class.getResource("docbook.rng").toString());
 
-  public static ConformityChecker usingDefaults() throws XmlException {
-    return cc(SCHEMA_FACTORY, SCHEMA_SOURCE);
+  /**
+   * Provides a DocBook conformity checker that uses the default factory and the embedded RNG
+   * DocBook schema.
+   * <p>
+   * It is recommended to embed your own RNG schema and use the following code for deciding which
+   * schema is used (and which factory). {@code
+   * SchemaHelper schemaHelper = SchemaHelper.schemaHelper(schemaFactory);
+   * Schema schema = schemaHelper.asSchema(schemaSource);
+   * return schemaHelper.conformityChecker(schema);
+   * }
+   *
+   * @return a conformity checker
+   * @see SchemaHelper
+   * @see XMLSyntaxSchemaFactory
+   */
+  public static ConformityChecker usingDefaults() {
+    final ConformityChecker cc;
+    try {
+      cc = cc(new XMLSyntaxSchemaFactory(), SCHEMA_SOURCE);
+    } catch (XmlException e) {
+      throw new VerifyException(e);
+    }
+    return cc;
   }
 
-  static ConformityChecker cc(SchemaFactory schemaFactory, StreamSource schemaSource) {
+  static ConformityChecker cc(SchemaFactory schemaFactory, StreamSource schemaSource)
+      throws XmlException {
     final SchemaHelper schemaHelper = SchemaHelper.schemaHelper(schemaFactory);
     final Schema schema = schemaHelper.asSchema(schemaSource);
     return schemaHelper.conformityChecker(schema);
