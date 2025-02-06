c4bbd178e694972c864eba95f2afc390ddc65fdb
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-02-06 23:22:00 +0100

    test pdf with details

--- src/test/java/io/github/oliviercailloux/publish/FoToPdfTransformerTests.java
+++ src/test/java/io/github/oliviercailloux/publish/FoToPdfTransformerTests.java
@@ -147,4 +147,66 @@ public class FoToPdfTransformerTests {
       }
     }
   }
+  @Test
+  void testArticleWithPdfDetails() throws Exception {
+    URL input = DocBookTransformerTests.class.getResource("Include PDF complex.fo");
+    final StreamSource source = new StreamSource(input.toString());
+    try (ByteArrayOutputStream destination = new ByteArrayOutputStream()) {
+      URI fopBaseUri = PathUtils.fromResource(FoToPdfTransformerTests.class, ".").path().toUri();
+      LOGGER.info("Base: {}.", fopBaseUri);
+      TransformerFactory factory = KnownFactory.XALAN.factory();
+      final URL configUrl = DocBookTransformer.class.getResource("fop-config.xml");
+      final FopFactory fopFactory;
+      try (InputStream configStream = configUrl.openStream()) {
+        fopFactory = FopFactory.newInstance(fopBaseUri, configStream);
+      }
+      final XmlTransformer delegateTransformer = XmlTransformer.usingFactory(factory);
+
+      final FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+      // foUserAgent.getEventBroadcaster().addEventListener(new LoggingEventListener());
+
+      final FoEventListener l = new FoEventListener();
+      foUserAgent.getEventBroadcaster().addEventListener(l);
+
+      final Result res;
+      try {
+        final Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, destination);
+        res = new SAXResult(fop.getDefaultHandler());
+      } catch (FOPException e) {
+        throw new IllegalStateException(e);
+      }
+
+      delegateTransformer.usingEmptyStylesheet().transform(source, res);
+      /*
+       * This duplicates the serious event that will get thrown in the log, but we’d better do that
+       * so that one can see the order of events in the log and thus where the first serious one
+       * happened exactly.
+       */
+      l.logAll();
+      assertEquals(1, l.seriouses().size());
+      Event serious = Iterables.getOnlyElement(l.seriouses());
+      LOGGER.error("First serious event: {}.", EventFormatter.format(serious));
+      Set<String> pKeys = serious.getParams().keySet();
+      LOGGER.error("Keys: {}.", pKeys);
+      String eventGroupID = serious.getEventGroupID();
+      LOGGER.error("Event group ID: {}.", eventGroupID);
+      String eventID = serious.getEventID();
+      String key = Iterables.getOnlyElement(pKeys);
+      RuntimeException value = (RuntimeException) serious.getParam(key);
+      LOGGER.error("Value.", value);
+      // l.seriouses().stream().findFirst().ifPresent(e -> {
+      // throw FoEventListener.asException(e);
+      // });
+      final byte[] pdf = destination.toByteArray();
+      assertTrue(pdf.length >= 10);
+      Files.write(Path.of("Include PDF.pdf"), pdf);
+      try (PDDocument document = PDDocument.load(pdf)) {
+        final int numberOfPages = document.getNumberOfPages();
+        assertEquals(1, numberOfPages);
+        final PDFTextStripper stripper = new PDFTextStripper();
+        final String text = stripper.getText(document);
+        assertTrue(text.contains("Hello"));
+      }
+    }
+  }
 }
